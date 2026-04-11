4eeda52461a6388f02073e379df5c696913f029b
RodolfoPissini <rodolfo.pissini@outlook.com>
2026-04-11 18:15:19 -0300

    fix: ajustar docker para render

--- config/puma.rb
+++ config/puma.rb
@@ -27,15 +27,19 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
 
-# Allow puma to be restarted by `bin/rails restart` command.
+environment ENV.fetch("RAILS_ENV", "production")
+
+# IMPORTANTE para Render Free
+workers ENV.fetch("WEB_CONCURRENCY", 1)
+
+# Preload ajuda a economizar memória
+preload_app!
+
 plugin :tmp_restart
 
-# Run the Solid Queue supervisor inside of Puma for single-server deployments
+# Só ativa se você realmente estiver usando
 plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 
-# Specify the PID file. Defaults to tmp/pids/server.pid in development.
-# In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]

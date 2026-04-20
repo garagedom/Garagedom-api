8d6c4cb2beb9cf9e6f50ebdc932278cb1d753d7f
RodolfoPissini <rodolfo.pissini@outlook.com>
2026-04-20 13:04:13 -0300

    ajuste landing page

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,12 @@ module Api
   module V1
     class UsersController < ApplicationController
       def me
-        render json: { id: current_user.id, email: current_user.email }, status: :ok
+        render json: {
+          id: current_user.id,
+          email: current_user.email,
+          profile_id: current_profile&.id,
+          profile_type: current_profile&.profile_type,
+        }, status: :ok
       end
     end
   end
